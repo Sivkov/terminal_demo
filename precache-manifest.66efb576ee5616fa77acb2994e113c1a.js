b8012f3d5bb272616f8f93beda6762a8d086b5da
Alex Sivkov <202493@mail.ru>
2019-11-06 18:15:53 +0400

    initial commit

---
+++ precache-manifest.66efb576ee5616fa77acb2994e113c1a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd5927f0e9d0f78ad36aa56bd6fbb2dd",
+    "url": "/Sivkov/terminal_demo/index.html"
+  },
+  {
+    "revision": "2d4db20105f3a92548ae",
+    "url": "/Sivkov/terminal_demo/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "9d12055ad5fb0a0abd73",
+    "url": "/Sivkov/terminal_demo/static/js/2.d703598d.chunk.js"
+  },
+  {
+    "revision": "2d4db20105f3a92548ae",
+    "url": "/Sivkov/terminal_demo/static/js/main.45c4af61.chunk.js"
+  },
+  {
+    "revision": "e20eec58a0e76cdd1324",
+    "url": "/Sivkov/terminal_demo/static/js/runtime-main.92a517df.js"
+  }
+]);
